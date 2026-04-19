c05c97ec9b25752d03cf821ecac63e7fcd3b0c06
11249A405 <11249a405@kanchiuniv.ac.in>
2026-04-19 13:47:52 +0530

    Add output comment to Factorial function
    Added output comment for clarity.

---
+++ Factorial(Recursive).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fact(int n) {
+    if(n == 0 || n == 1)
+        return 1;
+    return n * fact(n-1);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Factorial = %d\n", fact(n));
+
+    return 0;
+}
+/*
+Output:
+Enter a number: 5
+Factorial = 120
+*/
